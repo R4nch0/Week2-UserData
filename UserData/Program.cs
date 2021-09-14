61df57606a6f03a7fb01856bcee4dc78b8603555
Kevin <kkivikas@tktk.com>
2021-09-14 12:59:23 +0300

    Control Flow

---
+++ UserData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*programm küsib kasutaja sünniaastat;
+             *programm arvutab kasutaja vanust;
+             *programm kuvab kasutaja vanust konsoolis;
+             */
+
+            Console.WriteLine("Palun, sisesta oma sünniaasta:");
+            int yearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int userAge = 2021 - yearOfBirth;
+
+            // 1 Võimalus
+            //Console.WriteLine("Oled " + userAge + " aastat vana.");
+
+            // 2 Võimalus
+            //string interpolation
+            Console.WriteLine($"Oled {userAge} aastat vana.");
+        }
+    }
+}
